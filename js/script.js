ac02825ef6ae48562d141f5712e47f56104beb1a
syu0821 <159214938+syu0821@users.noreply.github.com>
2024-02-08 07:17:39 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+//　ハンバーガーボタンとドロワー
+$("#js-button-drawer").on("click", function () {
+  $(this).toggleClass("is-checked");
+  $("#js-drawer").slideToggle();
+  $("body").toggleClass("in-fixed");
+});
 
